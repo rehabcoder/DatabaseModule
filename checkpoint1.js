431269526fc6aaf7fcafb3560b1e93851e4254a3
unknown <akshay.saini@eaglecrk.com>
2018-05-07 19:09:44 -0400

    checkpoint 1 redone

---
+++ checkpoint1.js
@@ -0,0 +1,48 @@
+//checkpoint 1
+//1.)
+//>  what type of datatype is each?
+//A:	 string
+//A:	integer
+//A:	date
+//A:	float
+//A:	float
+
+//2.)
+//A: A database will be of better use when needing to quickly store and change a large amount of data.
+//A text file would be of better use for JSON data.
+//3.)
+//> A difference between SQL and other programming languages.
+
+//A: The main difference is that SQL is a declarative language where as other programming languages are procedural.
+//4.)
+//> Explain how the pieces of a database system fit together at a high level.
+
+//A: A database is generally outlined in a table that contains columns and rows. It is not readable and can be changed and manipulated.
+//5.)
+//>The meaning of table, row, column and value:
+
+//A: Columns run top to bottom where as rows run left to right. The values are the individual cells.
+//6.)
+//>Three data types that can be used in a table.
+
+//A: Integer
+//A: float
+//A: string
+//7.)
+//> Description of queries and results:
+
+//A: this query will return all payment transcations from Mega Foods that are under 500.
+//8.)
+//> Write SQL quieries.
+
+//A:SELECT email, signup
+//FROM users
+//WHERE name = 'DeAndre Data'
+
+//SELECT userid
+//FROM users
+//WHERE email = 'aleesia.algorithm@uw.edu'
+
+//Select *
+//FROM users
+//WHERE userid = '4'
